72a5bbab6ea20ab0c37f8616b09238e2d87e29c8
stanley akyea <stanley.akyea@gmail.com>
2019-11-11 22:27:07 +0100

    Set state variables for audio controls

--- src/index.js
+++ src/index.js
@@ -16,3 +16,31 @@ const Control = ({
     <button onClick={onIncrease}>+</button>
   </div>
 );
+
+// This version of the component stores its state as 4 separate variables
+const AudioControlsWithMultipleVariables = () => {
+  const [volume, setVolume] = useState(47);
+  const [treble, setTreble] = useState(13);
+  const [mid, setMid] = useState(35);
+  const [bass, setBass] = useState(50);
+
+  return (
+    <div className="audio-controls">
+      <Control
+        value={volume}
+        onIncrease={() => setVolume(volume + 1)}
+        onDecrease={() => setVolume(volume - 1)}
+      >
+        VOLUME
+      </Control>
+    </div>
+  );
+};
+
+ReactDOM.render(
+  <>
+    <h1>With Multiple Variables</h1>
+    <AudioControlsWithMultipleVariables />
+  </>,
+  document.querySelector('#root')
+);
